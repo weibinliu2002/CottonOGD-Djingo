e73d4d12cbacfbc58adb10aafca4c8ddcfe277a8
liuweiin2002 <2102394487@qq.com>
2026-01-29 19:35:16 +0800

    update Script

--- Script/Start.sh
+++ Script/Start.sh
@@ -2,4 +2,4 @@
 Manage=/data/web/CottonOGD/OGD/backend/manage.py
 Venv=/data/web/CottonOGD/OGD/backend/venv/Scripts/activate
 source $Venv
-python3 $Manage runserver 0.0.0:8000
+python3 $Manage runserver 0.0.0.0:8000
--- Script/script/genemaster.R
+++ Script/script/genemaster.R
@@ -4,15 +4,37 @@ genome<-args[1]
 gff_file<-args[2]
 print(paste0('gff文件：',gff_file))
 gffs<-rtracklayer::import(gff_file)
-gffs<-gffs[gffs$type=='gene']
-print(paste0('基因数：',length(gffs)))
+gffs_gene<-gffs[gffs$type=='gene']
+print(paste0('基因数：',length(gffs_gene)))
 data<-data.frame(
-    geneid=ifelse(!(is.na(gffs$ID)),gffs$ID,ifelse(!(is.na(gffs$Name)),gffs$Name,gffs$NAME)),
+    geneid=ifelse(!(is.na(gffs_gene$ID)),gffs_gene$ID,ifelse(!(is.na(gffs_gene$Name)),gffs_gene$Name,gffs_gene$NAME)),
     genome_id=genome
     )
 data$alias<-paste(data$genome_id,data$geneid,sep = '_')
-write.csv(data, paste0(genome,'_genemaster.csv'), row.names = FALSE,  quote = FALSE)
-data<-read.csv(paste0(genome,'_genemaster.csv'),header = T)
+
+gffss<-read.table(gff_file,header = F,sep = '\t')
+gffss<-dplyr::arrange(gffss,V1,V4,V5)
+gffs<-as.data.frame(gffs)|>dplyr::arrange(seqnames,start,end)
+
+data2<-data.frame(
+    
+    seqid=gffss$V1,
+    start=gffss$V4,
+    end=gffss$V5,
+    strand=gffss$V7,
+    source=gffss$V2,
+    type=gffss$V3,
+    value=gffss$V8,
+    phase=gffss$V6,
+    attribute=gffss$V9,
+    geneid=ifelse(!(is.na(gffs$ID)),gffs$ID,ifelse(!(is.na(gffs$Name)),gffs$Name,gffs$NAME)),
+    genome_id=genome
+    )
+    names(data2)<-c('seqid','start','end','strand','source','type','value','phase','attributes','geneid_id','genome_id')
+data2$geneid_id<-gsub('\\.\\d+\\..*$','',data2$geneid_id)
+data2$geneid_id<-gsub('\\.\\d+$','',data2$geneid_id)
+data$alias<-paste(data$genome_id,data$geneid,sep = '_')
+
 print(str(data))
 print('连接数据库')
 library(DBI)
@@ -26,7 +48,19 @@ con <- dbConnect(RMySQL::MySQL(),
                  password = "1234",
                  )
 print(paste0('数据表：',dbListTables(con)))
-dbWriteTable(con,'genemaster',data,overwrite=F,append=T,row.names=FALSE)
+#dbWriteTable(con,'genemaster',data,overwrite=F,append=T,row.names=FALSE)
 print('写入数据库成功')
+genemaster<-dbReadTable(con,'genemaster',filter = paste0('genome_id="',genome,'"'))
+
+data2 <- data2 |>
+  dplyr::left_join(
+    genemaster |> dplyr::select(geneid, genome_id,id),
+    by = c("geneid_id" = "geneid", "genome_id" = "genome_id")
+  ) 
+names(data2)[12]<-c('id_id')
+str(data2)  
+dbWriteTable(con,'gene_assembly',data2,overwrite=F,append=T,row.names=FALSE)
+
 DBI::dbDisconnect(con)
 print('断开数据库连接')
+
