fe1b7865a33fcec87a4a68f50b85f71b6cc263ae
liuweibin2002 <2102394487@qq.com>
2026-03-06 12:00:02 +0800

    Auto commit on 2026-03-06 12:00:02

--- test_rpy2.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['R_HOME'] = r'D:\Program Files\R\R-4.5.2'
-
-import rpy2.situation as rpy2_situation
-rpy2_situation.R_HOME = r'D:\Program Files\R\R-4.5.2'
-
-import rpy2.rinterface as rinterface
-rinterface.initr()
-print('R initialization successful!')
